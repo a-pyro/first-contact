ab5aa2df30a087ce02e542739b272814d837d520
Ardi Germenji <ardi.germenji@gmail.com>
2024-07-09 17:44:06 +0100

    renames and moves stuff around

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,63 +1,7 @@
-/* eslint-disable react/no-unstable-nested-components -- rn */
-import { Tabs } from 'expo-router'
 import React from 'react'
 
-import {
-  BookmarkTabIcon,
-  CreateTabIcon,
-  HomeTabIcon,
-  ProfileTabIcon,
-} from '@/components/navigation/tab-bar-icon'
+import { TabsBar } from '@/components'
 
-const TabsLayout = () => {
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarShowLabel: false, // only show the label of the icon
-      }}
-    >
-      <Tabs.Screen
-        name="home" // the name of the screen (home.tsx)
-        options={{
-          title: 'home',
-          headerShown: false,
-          tabBarIcon: ({ color, focused }) => (
-            <HomeTabIcon color={color} focused={focused} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="create"
-        options={{
-          title: 'create',
-          headerShown: false,
-          tabBarIcon: ({ color, focused }) => (
-            <CreateTabIcon color={color} focused={focused} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'profile',
-          headerShown: false,
-          tabBarIcon: ({ color, focused }) => (
-            <ProfileTabIcon color={color} focused={focused} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="bookmark"
-        options={{
-          title: 'bookmark',
-          headerShown: false,
-          tabBarIcon: ({ color, focused }) => (
-            <BookmarkTabIcon color={color} focused={focused} />
-          ),
-        }}
-      />
-    </Tabs>
-  )
-}
+const TabsLayout = () => <TabsBar />
 
 export default TabsLayout
