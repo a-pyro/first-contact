bacc38be38e7ca2a88c47a429834c24c838c80a3
Ardi Germenji <ardi.germenji@gmail.com>
2024-07-11 14:34:25 +0100

    feat: Add package name to iOS configuration in app.json

---
+++ utils/app-write.ts
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+  endpoint: 'https://cloud.appwrite.io/v1',
+  platform: 'com.apyro.rnfirstcontact',
+  projectId: '668fdc1d00385c1e0373',
+  databaseId: '668fdd990038c4cbb23e',
+  storageId: '668fdef900373ee2a145',
+  userCollectionId: '668fddb2003ccd7400ce',
+  videoCollectionId: '668fdd990038c4cbb23e',
+}
--- utils/index.ts
+++ utils/index.ts
@@ -1,1 +1,2 @@
+export * from './app-write'
 export * from './ui'
